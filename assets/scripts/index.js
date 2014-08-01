591bc89de8971d236af6d9b3ebc492526879b8c4
Lee <jessy1092@gmail.com>
2014-08-02 02:01:07 +0800

    Add front-end 3 slides.
    Signed-off-by: Lee <jessy1092@gmail.com>

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
     impress().init();
+
+    $("#test_header").hover( function () {
+        $(this).css('color', 'blue');
+    }, function () {
+        $(this).css('color', 'red');
+    });
 });
